8b651dadc51bb1ea4653f6331845c35853ffe2ca
Icefrost <56258766+LycanIcefrost@users.noreply.github.com>
2021-03-02 11:01:27 +0700

    Create tugas1.cpp

---
+++ tugas1.cpp
@@ -0,0 +1,17 @@
+//Program akan dibuat dalam bahasa pemrograman C++
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 5;
+    int y = 4;
+
+    //Penjumlahan
+    int z = x + y;
+
+    cout << "Program Penjumlahan\n\n";
+    cout << "Nilai X = " << x << "\n";
+    cout << "Nilai Y = " << y << "\n";
+    cout << "Hasil Penjumlahan = " << z << "\n";
+    return 0;
+}
